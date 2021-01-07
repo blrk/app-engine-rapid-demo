8667d0342242a0b91a40d9bd3d0bf556cd07537b
blrk <blradhakrishnan@gmail.com>
2021-01-07 16:15:35 +0530

    update hello world

--- python3.7/main.py
+++ python3.7/main.py
@@ -12,35 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START gae_python38_render_template]
-# [START gae_python3_render_template]
-import datetime
+# [START gae_python38_app]
+# [START gae_python3_app]
+from flask import Flask
 
-from flask import Flask, render_template
 
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
 app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
-
-    return render_template('index.html', times=dummy_times)
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_render_template]
-# [END gae_python38_render_template]
+# [END gae_python3_app]
+# [END gae_python38_app]
